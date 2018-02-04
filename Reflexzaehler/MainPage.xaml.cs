f5b3e134f93844ab19170b849a7428ca74504161
Martin Kamps <martin@kamps.name>
2018-02-04 15:56:35 +0100

    SPI hinzugefügt

--- Reflexzaehler/MainPage.xaml.cs
+++ Reflexzaehler/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -12,18 +13,19 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-using Windows.Devices.Gpio;
 using Windows.UI.Xaml.Shapes;
 
-using ThingSpeakWinRT;
-using System.Threading.Tasks;
+using Windows.Devices.Gpio;
+using Windows.Devices.Spi;
+using Windows.Devices.Enumeration;
 
-// Die Elementvorlage "Leere Seite" wird unter https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x407 dokumentiert.
+
+// using ThingSpeakWinRT;
+// using System.Threading.Tasks;
 
 namespace Reflexzaehler
 {
-
-
+    
     /// <summary>
     /// Eine leere Seite, die eigenständig verwendet oder zu der innerhalb eines Rahmens navigiert werden kann.
     /// </summary>
@@ -33,19 +35,24 @@ namespace Reflexzaehler
         private GpioController gpio;
         private GpioPin pinLED_Active;
         private GpioPinValue pinLED_Active_Value;
+
+        private SpiDevice spiDevice;
+
         private DispatcherTimer timer;
         private SolidColorBrush redBrush = new SolidColorBrush(Windows.UI.Colors.Red);
         private SolidColorBrush grayBrush = new SolidColorBrush(Windows.UI.Colors.LightGray);
         private int state = 0;
 
-        private ThingSpeakClient theThingspeakClient = new ThingSpeakClient(false);
+        //private ThingSpeakClient theThingspeakClient = new ThingSpeakClient(false);
 
         public MainPage()
         {
             int iErr = 0;
 
             this.InitializeComponent();
-                        
+            Loaded += MainPage_Loaded;
+            
+
             // create timer
             timer = new DispatcherTimer();
             timer.Interval = TimeSpan.FromMilliseconds(500);
@@ -63,12 +70,39 @@ namespace Reflexzaehler
 
         }
 
-         public async void SendToThingspeak()
+        private async void MainPage_Loaded(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            string selector = SpiDevice.GetDeviceSelector();
+            IReadOnlyList<DeviceInformation> devices = await DeviceInformation.FindAllAsync(selector);
+
+            if( devices.Count > 0 )
+            {
+                // SPI settings
+                var settings = new SpiConnectionSettings(0);
+                settings.ClockFrequency = 5000000;
+                settings.Mode = SpiMode.Mode3;
+
+                spiDevice = await SpiDevice.FromIdAsync(devices[0].Id, settings);
+                if( spiDevice == null)
+                {
+                    Debug.WriteLine("SPI0 in use");
+                    Application.Current.Exit();
+                }
+            }
+            else
+            {
+                Debug.WriteLine("no SPI device");
+                Application.Current.Exit();
+            }
+        }
+
+        /* public async void SendToThingspeak()
         {
             ThingSpeakFeed dataFeed = new ThingSpeakFeed { Field1 = "58.27", Field2 = "32.59" };
             dataFeed = await theThingspeakClient.UpdateFeedAsync("<Your Write API Key>", dataFeed);
 
         }
+        */
 
         private int InitStateLED(int pin)
         {
@@ -104,9 +138,33 @@ namespace Reflexzaehler
             }
         }
 
+        public int ReadValueFromMCP3002(byte channelNumber)
+        {
+            byte[] readBuffer = new byte[2];
+            byte[] writeBuffer = new byte[2];
+
+            // 0x40: start bit (0b0100_0000, 7. bit)
+            // 0x20: single ended (0b0010_0000, 6. bit)
+            // 0x10: channel=1 (0b0001_0000, 5. bit)
+            // 0x08: MSB (0b0000_1000, 4. bit)
+            writeBuffer[0] = (byte)(0x40 | 0x20);
+            if (channelNumber == 1) writeBuffer[0] |= 0x10;
+
+            spiDevice.TransferFullDuplex(writeBuffer, readBuffer);
+
+            // byte 0: only 2 bits 
+            var value = (readBuffer[0] & 0x03) << 8;
+            // byte 1: all 8 bits
+            value |= readBuffer[1];
+            
+            return value;
+        }
+
         private void Timer_Tick(object sender, object e)
         {
+            int adcValue;
 
+            adcValue = ReadValueFromMCP3002(0); // IR photo transistor @ channel 0 of MCP 3002
 
 
             // trigger state LED
